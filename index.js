295792c7b1280fefba87fe298eef81975bc3d51b
Charles Boyce <wxaz020@live.rhul.ac.uk>
2018-01-01 16:45:58 +0000

    FIX: fixed a URL encoding issue when redirected from a youtube playlist

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    const url = tabs[0].url.split('watch?v=');
+    let url = tabs[0].url.split('watch?v=');
+    url[1].includes('&') ? url = url[1].split('&') : url = url[1];
     (url[0] === "https://www.youtube.com/")
-      && chrome.tabs.create({ url: "http://notesense.herokuapp.com/single/" + url[1]});
+      && chrome.tabs.create({ url: "http://notesense.herokuapp.com/single/" + url});
   });
 });
 
