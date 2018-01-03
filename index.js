d78267820f12f4aefd99eaf0c40b5599a13ec0ec
Charles Boyce <wxaz020@live.rhul.ac.uk>
2018-01-03 20:55:47 +0000

    FIX: actually fixed this time, apologies

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
   chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    let url = tabs[0].url.split('watch?v=');
-    url[1].includes('&') ? url = url[1].split('&') : url = url[1];
-    (url[0] === "https://www.youtube.com/")
-      && chrome.tabs.create({ url: "http://notesense.herokuapp.com/single/" + url});
+    if (tabs[0].url.includes('https://www.youtube.com/')) {
+      let url = tabs[0].url.split('watch?v=');
+      url[1].includes('&') ? url = url[1].split('&') : url = url[1];
+      chrome.tabs.create({ url: "http://notesense.herokuapp.com/single/" + url[0]});  
+    }
   });
 });
 
